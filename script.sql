c2a7b11c27d872c41807fa5e6d7248cfe7a0aed2
Mehak Goel <mhkgoel29@gmail.com>
2025-04-28 13:06:22 +0530

    day1

---
+++ script.sql
@@ -0,0 +1,25 @@
+/*SQL (Structured Query Language):
+SQL is a standard programming language specifically designed for 
+managing and manipulating databases. It allows users to interact with
+ relational databases to perform tasks such as querying, updating,
+ inserting, and deleting data. */
+ 
+ 
+select * from ecommerce.customers;
+select customer_id,customer_city, customer_state from ecommerce.customers;
+-- select statement is for retreiving columns
+-- * means everything
+
+/*
+The WHERE clause is used in SQL to filter records that meet specific 
+conditions. It is typically used in combination with SELECT, UPDATE, DELETE, 
+and other SQL commands to specify which records you want to work with.
+*/
+select * from customers where customer_state="MG";
+select * from orders where order_status="canceled";
+select * from payments where (payment_type="UPI" or payment_type="credit_card") and payment_value>=1000;
+
+/* NOT has the highest priority and will be evaluated first.
+AND has a higher precedence than OR, so it will be evaluated before OR when no brackets are used.
+OR has the lowest precedence, so it will be evaluated last unless brackets are used to explicitly change the order.*/
+
